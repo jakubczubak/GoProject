db496d0715506487533d5e65ef338a34ed871d3f
Jakub Czubak <czubakjakub94@gmail.com>
2020-04-10 14:58:42 +0200

    crete dashboard.html

--- src/main/java/com/jczubak/crm/controller/HomeController.java
+++ src/main/java/com/jczubak/crm/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("")
     public String getHomePage(){
-        return "index";
+        return "dashboard";
     }
 }
--- src/main/java/com/jczubak/crm/service/ActivityService.java
+++ src/main/java/com/jczubak/crm/service/ActivityService.java
@@ -1,7 +1,31 @@
 package com.jczubak.crm.service;
 
+import com.jczubak.crm.entity.Activity;
+import com.jczubak.crm.repository.ActivityRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActivityService {
+
+    private ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository){
+        this.activityRepository=activityRepository;
+    }
+
+    public void addActivity(Activity activity){
+        activityRepository.save(activity);
+    }
+
+    public void removeActivity(Long id){
+        activityRepository.deleteById(id);
+    }
+
+    public List<Activity> getListOdActivity(){
+        return activityRepository.findAll();
+    }
+
+
 }
