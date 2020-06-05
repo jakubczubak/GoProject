10b7e66086ca2c25ff657e4bae01cf4b9460985a
Jakub Czubak <czubakjakub94@gmail.com>
2020-06-05 19:37:48 +0200

    add simpleTest

--- src/main/java/com/jczubak/crm/CrmApplication.java
+++ src/main/java/com/jczubak/crm/CrmApplication.java
@@ -3,12 +3,20 @@ package com.jczubak.crm;
 import com.jczubak.crm.service.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
 
+
+    @GetMapping("/simpleTest")
+    public String doSimpeTest(){
+        return "simpleTest";
+    }
 }
