0b411d48185cbebee2c233df7a32bb68e245692f
Jakub Czubak <czubakjakub94@gmail.com>
2020-06-17 10:03:05 +0200

    add loader to webapp

---
+++ src/main/resources/static/js/loader.js
@@ -0,0 +1,6 @@
+var spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', function () {
+    // spinnerWrapper.style.display = 'none';
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+});
